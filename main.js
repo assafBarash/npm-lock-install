84b19440c18588e6c19335defaad8d414ce6a1fe
barash <assafba@wix.com>
2021-08-24 13:27:46 +0300

    fixed lookup invocation

--- main.js
+++ main.js
@@ -3,13 +3,10 @@ const { execSync } = require('child_process');
 const { lookup } = require('./utils');
 
 function main(packages = '', { debug }) {
-  if (debug) {
-    console.log('cwd', process.cwd());
-    console.log('__dirname', __dirname);
-  }
+  if (debug) console.log('cwd', process.cwd());
   if (!packages) throw Error('package/s arg must be included');
 
-  const npmrcFileDir = lookup({ file: '.npmrcFile', debug });
+  const npmrcFileDir = lookup({ file: '.npmrc', debug });
 
   if (!npmrcFileDir) throw Error("couldn't locate .npmrc file to update");
 
